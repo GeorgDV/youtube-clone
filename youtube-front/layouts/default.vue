7f550054ea912985593eee5808396324da557588
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2021-03-09 11:53:12 +0200

    Initialize

---
+++ youtube-front/layouts/default.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      value="true"
+      :mini-variant="isMiniVariant"
+      :clipped="true"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="true"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="isMiniVariant = !isMiniVariant" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'YouTube',
+      isMiniVariant: false,
+      items: [
+        {
+          icon: 'mdi-home',
+          title: 'Home',
+          to: '/'
+        },
+        {
+          icon: 'mdi-fire',
+          title: 'Trending',
+          to: '/trending'
+        },
+        {
+          icon: 'mdi-youtube-subscription',
+          title: 'Subscriptions',
+          to: '/subscription'
+        }
+      ],
+    }
+  }
+}
+</script>
