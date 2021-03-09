d35169b8106a78c6707d0b77ff1585aeff479df5
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2021-03-09 12:10:42 +0200

    Add List component for showing items as list

---
+++ youtube-front/components/List.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-list>
+    <v-list-item
+      v-for="(item, i) in items"
+      :key="i"
+      :to="item.to"
+      router
+      exact
+    >
+      <v-list-item-action>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title v-text="item.title" />
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {
+name: "List.vue",
+  props: {
+    items: [],
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
